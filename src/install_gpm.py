74d18592f9ff52050af80c9f31bf30fcc99737c7
Gavin TC <gtclonts@gmail.com>
2024-03-26 15:43:03 -0400

    finished installer, just have to check compatibility with mac

--- src/install_gpm.py
+++ src/install_gpm.py
@@ -1,9 +1,41 @@
 import os
 import sys
 import subprocess
+import PyInstaller.__main__
 
-cur_dir = os.path.dirname(os.path.realpath(__file__))
-command = 'python -m PyInstaller --distpath ' + cur_dir + ' --clean --noconfirm --onefile --console --name "gpm"  "' + cur_dir + '\\main.py"'
+def main():
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    install_dir = ""
+    path_dir = ""
 
-os.system(command)
+    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+        install_dir = "/usr/local/bin/"
+        path_dir = install_dir + "/gpm"
+    elif sys.platform.startswith('win'):
+        install_dir = "C:\\Program Files"
+        path_dir = install_dir + "\\gpm"
+    else:
+        print("[ERROR] OS not supported!")
+        os.system("PAUSE")
+        return
+
+
+    print("\n[\u001b[33mWARNING\u001b[0m] PLEASE DO NOT CLOSE THIS WINDOW UNTIL INSTALLATION IS FINISHED!")
+
+    PyInstaller.__main__.run([
+        "--distpath", install_dir,
+        "--clean",
+        "--noconfirm",
+        "--console",
+        "--name", "gpm",
+        os.path.join(cur_dir, "main.py")
+    ])
+
+    if sys.platform.startswith('win'):
+        print("\n[\u001b[33mWARNING\u001b[0m] If you haven't already, add the following to your PATH environment variable or the program will not work:")
+        print("[\u001b[33m" + path_dir + "\u001b[0m] (COPIED TO CLIPBOARD)\n")
+        os.system('echo ' + path_dir + '| clip')
+        os.system("PAUSE")
+
+main()
 
