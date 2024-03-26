927a4b6ae9f895ed07bb8987fb5830280d6964ad
Gavin TC <gtclonts@gmail.com>
2024-03-26 15:08:31 -0400

    adjusted .gitignore + finishing up installer

--- src/install_gpm.py
+++ src/install_gpm.py
@@ -3,10 +3,7 @@ import sys
 import subprocess
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
-command = 'pyinstaller --noconfirm --onefile --console --name "gpm"  "' + cur_dir + '"'
-
-print(command)
-os.system("PAUSE")
+command = 'python -m PyInstaller --distpath ' + cur_dir + ' --clean --noconfirm --onefile --console --name "gpm"  "' + cur_dir + '\\main.py"'
 
 os.system(command)
 
