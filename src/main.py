6a65e33637ac46913ae0b96b249bf8f942f159af
Gavin TC <gtclonts@gmail.com>
2024-03-26 02:21:33 -0400

    added flags -a, -r, -l, -h

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import random
 import csv
 # from Crypto.Cipher import AES
@@ -9,23 +10,123 @@ file_name: str = 'pswd.csv'
 
 def main() -> None:
     if len(sys.argv) < 2:
+        print("Use: [gpm -h] for help.")
         return
+    else:
+        match sys.argv[1]:
+            case "-c":  # create/change passwords flag
+                if len(sys.argv) < 3:
+                    print("[ERROR] Missing name argument!")
+                    print("To create a password use: [gpm -c \"password_name\"]")
+                    print()
+                    return
+                create_password(sys.argv[2])
+                print()
+            
+            case "-a":  # add existing password flag
+                if len(sys.argv) < 4:
+                    print("[ERROR] Missing an argument!")
+                    print("To add a password use: [gpm -a \"password_name\" \"password\"]")
+                    print()
+                    return
+                add_password(sys.argv[2], sys.argv[3])
+                print()
+            
+            case "-r":  # remove password flag
+                if len(sys.argv) < 3:
+                    print("[ERROR] Missing name argument!")
+                    print("To remove a password use: [gpm -r \"password_name\"]")
+                    print()
+                    return
+                remove_password(sys.argv[2])
+                print()
+                
+            case "-l":  # list passwords flag
+                with open(file_name, 'r') as file:
+                    reader = csv.reader(file)
+                    rows = list(reader)
 
-    match sys.argv[1]:
-        case "-c":
-            if len(sys.argv) < 3:
-                print("[ERROR] Missing name argument!")
-                print("To create a password use: gpm -c \"password_name\"")
+                    if len(rows) > 0:
+                        for row in rows:
+                            print(row[0] + ": " + row[1])
+                    else:
+                        print("[ERROR] There aren't any passwords stored!")
+                        print()
+                print()
+            
+            case "-h":  # help flag
+                print("Use \"-c\" flag to create/change a current password.")
+                print("Use [gpm \"password_name\"] to get the password associated with that name.")
+                print("Ctrl + C to cancel at any point during password creation.")
+                print()
+
+            case _:  # retrieve password 'flag'
+                get_password(sys.argv[1])
+                print()
+
+# -c FLAG
+def create_password(pass_name: str) -> None:
+    satisfied: bool = False
+    password: str = ""
+
+    while not satisfied:
+        length: int = abs(max(1, min(int(input("LENGTH?: ").lower()), 25)))
+        print(length)
+        numbers:  bool = True if input("NUMBERS?: [Y/n]: ").lower() == 'y' else False
+        letters:  bool = True if input("LETTERS?: [Y/n]: ").lower() == 'y' else False
+        specials: bool = True if input("SPECIAL?: [Y/n]: ").lower() == 'y' else False
+
+        if letters == False and numbers == False and specials == False:
+            print("[ERROR] You have to have atleast 1 type of character in your password!")
+            continue
+
+        password = generate_password(length, numbers, letters, specials)
+
+        print("\nYour password is: \"" + password + "\"")
+        satisfied = False if input("Remake? [Y/n]: ").lower() == 'y' else True
+    
+    store_password(sys.argv[2], password)
+
+
+# -a FLAG
+def add_password(pass_name: str, password: str) -> None:
+    if not exists(pass_name):
+        store_password(pass_name, password)
+    else:
+        print("[ERROR] That password already exists!")
+        print()
+
+
+# -r FLAG
+def remove_password(pass_name: str) -> None:
+    if exists(pass_name):
+        with open(file_name, 'r') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+
+        rows = [row for row in rows if row[0] != pass_name]
+
+        with open(file_name, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(rows)
+    else:
+        print("[ERROR] That password doesn't exist!")
+        print()
+
+
+# default FLAG
+def get_password(pass_name: str) -> None:
+    with open(file_name, 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+
+        for row in rows:
+            if row[0] == pass_name:
+                print(row[1])
+                os.system('echo '+ row[1] + '| clip')  # copy password
                 return
-            create_password(sys.argv[2])
-        case "-r":
-            if len(sys.argv) < 3:
-                print("[ERROR] Missing name argument!")
-                print("To replace a password do: 'gpm -r [password_name]'")
-                return
-            replace_password(sys.argv[2])
-        case _:
-            get_password(sys.argv[1])
+    print("[ERROR] That password doesn't exist!")
+    print()
 
 
 def generate_password(length: int, number: bool, letter: bool, special: bool) -> str:
@@ -36,7 +137,7 @@ def generate_password(length: int, number: bool, letter: bool, special: bool) ->
 
     numbers = "1234567890"
     letters = "abcdefghijklmnopqrstuvwxyz"
-    specials = "!?@#$%&*-_"
+    specials = "!?@#$%-*_"
     all_chars = numbers + letters + specials
 
     chosen_numbers = ""
@@ -66,47 +167,11 @@ def generate_password(length: int, number: bool, letter: bool, special: bool) ->
 
     # shuffle password length times, for fun
     for x in range(length):
-        password = shuffle_password(password)    
+        password_arr = list(password)
+        random.shuffle(password_arr)
+        password = ''.join(password_arr)
 
     return password
-
-
-def shuffle_password(password: str) -> str:
-    password_arr = list(password)
-    random.shuffle(password_arr)
-    return ''.join(password_arr)
-
-
-def get_password(pass_name: str) -> None:
-    with open(file_name, 'r') as file:
-        reader = csv.reader(file)
-        rows = list(reader)
-
-        for row in rows:
-            if row[0] == pass_name:
-                print(row[1])
-
-
-def create_password(pass_name: str) -> None:
-    satisfied: bool = False
-    password: str = ""
-
-    while not satisfied:
-        length: int = abs(int(max(1, min(ord(input("PASSWORD LENGTH: ").lower()[0]), 25))))
-        letters:  bool = True if input("LETTERS?: [Y/n]: ").lower() == 'y' else False
-        numbers:  bool = True if input("NUMBERS?: [Y/n]: ").lower() == 'y' else False
-        specials: bool = True if input("SPECIAL?: [Y/n]: ").lower() == 'y' else False
-
-        if letters == False and numbers == False and specials == False:
-            print("[ERROR] You have to have atleast 1 type of character in your password!")
-            continue
-
-        password = generate_password(length, letters, numbers, specials)
-
-        print("\nYour password is: \"" + password + "\"")
-        satisfied = False if input("Remake? [Y/n]: ").lower() == 'y' else True
-    
-    store_password(sys.argv[2], password)
 
 
 def store_password(name, password) -> None:
@@ -123,8 +188,8 @@ def store_password(name, password) -> None:
                         row[1] = password
                     writer.writerow(row)
         except Exception as e:
-            print("[ERROR] There was a problem reading the file!")
-            print(e)
+            print("[ERROR] There was a problem reading the file!: \"" + e + "\"")
+            print()
     else:
         with open(file_name, 'a', newline='') as file:
             writer = csv.writer(file)
@@ -136,41 +201,6 @@ def exists(name) -> bool:
         reader = csv.reader(file)
         return any(row[0] == name for row in reader)
         
-
-    
-def replace_password(pass_name: str) -> None:
-    print("Replacing password for", pass_name)
-
-
-# def encrypt_file(filename, key):
-#     data = ''
-#     with open(filename, 'r') as file:
-#         data = file.read()
-
-#     cipher = AES.new(key, AES.MODE_CBC)
-#     ciphertext = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
-
-#     with open(filename + '.enc', 'wb') as file:
-#         file.write(cipher.iv)
-#         file.write(ciphertext)
-
-
-# def decrypt_file(filename, key):
-#     with open(filename, 'rb') as file:
-#         iv = file.read(16)
-#         ciphertext = file.read()
-
-#     cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-#     plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
-
-#     with open(filename.replace('.enc', '.dec'), 'w') as file:
-#         file.write(plaintext.decode('utf-8'))
-
-# # Usage
-# key = get_random_bytes(16)
-# encrypt_file('passwords.csv', key)
-# decrypt_file('passwords.csv.enc', key)
-
 
 if __name__ == "__main__":
     main()
